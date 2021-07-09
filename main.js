7e7cbd6a725d1e12787b9370943eeef824f2e659
Kamogelo Mashike <marykamogelo387@gmail.com>
2021-07-09 16:18:34 +0200

    changes to website backtotopbuton added

---
+++ main.js
@@ -0,0 +1,18 @@
+let backToTopButton = document.querySelector("#back-to-top-btn");
+window.addEventListener("scroll",scrollFuction);
+
+function scrollFuction(){
+  if (window.pageYOffset>200){
+    backToTopButton.style.display = "block";
+  }
+else{
+  backToTopButton.style.display = "none";
+}
+}
+
+
+backToTopButton.addEventListener("click",backToTop);
+
+function backToTop(){
+  window.scrollTo(0,0);
+}
