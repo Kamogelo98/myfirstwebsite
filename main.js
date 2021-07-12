78409aae2e36297ff2ec5f039de628cac440843a
Kamogelo Mashike <marykamogelo387@gmail.com>
2021-07-12 17:23:07 +0200

    changes to website

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let backToTopButton = document.querySelector("#back-to-top-btn");
 window.addEventListener("scroll",scrollFuction);
 
 function scrollFuction(){
-  if (window.pageYOffset>200){
+  if (window.pageYOffset>300){
     backToTopButton.style.display = "block";
   }
 else{
@@ -15,4 +15,32 @@ backToTopButton.addEventListener("click",backToTop);
 
 function backToTop(){
   window.scrollTo(0,0);
+}
+
+
+
+
+function validateform(){
+  let userName= document.getElementById("name");
+  var userEmail= document.getElementById("email");
+  var userNum= document.getElementById("number");
+
+
+  if(userName == "" || userEmail == "" || userNum == ""){
+    alert("Please fill out the form!");
+    return false;
+  }else{
+  alert("You have successfully submmited your form!");
+  return true;
+  }
+}
+
+
+function validatesub(){
+  let userEmail=document.getElementsByClassName("email");
+  if(userEmail == ""){
+    alert("Please  enter your email to subscribe");
+  }else{
+    alert("You have successfully subscribed for our newsletter!");
+  }
 }
